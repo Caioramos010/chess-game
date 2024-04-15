2c000a1d98c5aa96c5e23d475cb04cc6a00cb951
Caio Ramos <caio.sr06@gmail.com>
2024-04-15 16:44:30 -0300

    Add pieces, table functions and exceptions

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pices.Rook;
 
 public class ChessMatch {
 
@@ -8,6 +10,7 @@ public class ChessMatch {
 
     public ChessMatch() {
         board = new Board(8, 8);
+        initialSetup();
     }
 
     public ChessPiece[][] getPieces() {
@@ -18,5 +21,13 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+
+    private void placeNewPiece(char column, int row, ChessPiece piece){
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
+    private void initialSetup(){
+        placeNewPiece('B', 6, new Rook(board, Color.WHITE));
     }
 }
